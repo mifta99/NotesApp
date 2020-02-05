7036fc13c4c6c204f3e37dbf7df2a556686bc2f4
miftaa <hasanahm341@gmail.com>
2020-02-05 08:49:19 +0700

    iam trying

--- app/src/main/java/com/mifta/project/id/dicodingnotesapp/db/DatabaseContract.java
+++ app/src/main/java/com/mifta/project/id/dicodingnotesapp/db/DatabaseContract.java
@@ -1,12 +1,24 @@
 package com.mifta.project.id.dicodingnotesapp.db;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class DatabaseContract {
-    static String TABLE_NAME = "Note";
+    public static final String AUTHORITY = "com.mifta.project.id.dicodingnotesapp";
+    private static final String SCHEME = "content";
+
+
+    private DatabaseContract(){}
+
     public static final class NoteColumns implements BaseColumns {
+        static String TABLE_NAME = "Note";
         public static String TITLE = "title";
         public static String DESCRIPTION = "description";
         public static String DATE = "date";
+
+        public static final Uri CONTENT_URI = new Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build();
     }
 }
